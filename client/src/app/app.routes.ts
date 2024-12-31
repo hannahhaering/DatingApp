934f88330efd8071f1794123d085336f12d1790f
hannahhaering <hannah.haering96@gmail.com>
2024-12-31 14:38:07 +0100

    Build user interface

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -15,8 +16,8 @@ export const routes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [authGuard],
         children: [
-            {path: 'members', component: MemberDetailComponent},
-            {path: 'members/:id', component: MemberDetailComponent},
+            {path: 'members', component: MemberListComponent},
+            {path: 'members/:username', component: MemberDetailComponent},
             {path: 'lists', component: ListsComponent},
             {path: 'messages', component: MessagesComponent},
         ]
